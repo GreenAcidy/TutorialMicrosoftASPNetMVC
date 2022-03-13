0a71c5c805637ebf60bc999cc618df0a64b08137
GreenAcidy <47273458+GreenAcidy@users.noreply.github.com>
2022-03-13 22:13:42 +0300

    Complete task 8

--- MvcMovie/MvcMovie/Models/SeedData.cs
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -26,6 +26,7 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
@@ -34,6 +35,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "S",
                         Price = 8.99M
                     },
 
@@ -42,13 +44,15 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "A"
                     },
 
                     new Movie
                     {
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Rating = "O",
                         Genre = "Western",
                         Price = 3.99M
                     }
